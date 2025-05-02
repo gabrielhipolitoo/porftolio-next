538ab43d058ee3ceeda99afd512e6124d65d6a21
gabrielhipolitoo <devgabrielhipolito>
2025-05-02 18:25:23 -0300

    add action button

--- src/pages/Acess/AcessProjetct.tsx
+++ src/pages/Acess/AcessProjetct.tsx
@@ -1,4 +1,5 @@
 import { Composition } from "@/components/section";
+import Link from "next/link";
 import React from "react";
 
 export default function AcessProjetct() {
@@ -9,18 +10,24 @@ export default function AcessProjetct() {
         <p className="text-2xl mt-10 font-light text-white">
           Conheça alguns dos meus projetos pessoais e profissionais
         </p>
-        <button className="p-5 mt-10 cursor-pointer bg-[#131316] text-white rounded-md hover:bg-[#6472CC] transition">
+        <Link
+          href={"/projects"}
+          className="p-5 mt-10 cursor-pointer bg-[#131316] text-center text-white rounded-md hover:bg-[#6472CC] transition"
+        >
           Acessar Projetos
-        </button>
+        </Link>
       </div>
       <div className="p-4 flex  h-full  flex-col justify-between  ">
         <h2 className="text-6xl text-white text-right">Leia meus artigos</h2>
         <p className="text-2xl mt-10 font-light text-white text-right">
           Artigos sobre design, arquiterua e clean code, entre outros.
         </p>
-        <button className="p-5 mt-10 cursor-pointer bg-[#131316] text-white rounded-md hover:bg-[#6472CC] transition">
-          Acessar artigos
-        </button>
+        <Link
+          href={"/artcles"}
+          className="p-5 mt-10 cursor-pointer bg-[#131316] text-center text-white rounded-md hover:bg-[#6472CC] transition"
+        >
+          Acessar Artigos
+        </Link>
       </div>
     </Composition.section>
   );
