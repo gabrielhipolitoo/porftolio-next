a3845a8fc7360931784ff165e85033289a8eeb96
gabrielhipolitoo <devgabrielhipolito>
2025-05-03 13:47:18 -0300

    font

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -3,15 +3,15 @@ import { Work_Sans } from "next/font/google";
 export const work_sans_bold = Work_Sans({
   weight: "700",
   style: ["normal"],
-  subsets: ["latin"],
+  subsets: ["latin", "latin-ext", "vietnamese"],
 });
 export const work_sans_normal = Work_Sans({
   weight: "400",
   style: ["normal"],
-  subsets: ["latin"],
+  subsets: ["latin", "latin-ext", "vietnamese"],
 });
 export const work_sans_light = Work_Sans({
   weight: "300",
   style: ["normal"],
-  subsets: ["latin"],
+  subsets: ["latin", "latin-ext", "vietnamese"],
 });
