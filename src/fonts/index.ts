43d90b02b8206e0ce8543a00948e9d036a0a265b
gabrielhipolitoo <devgabrielhipolito>
2025-05-03 13:24:04 -0300

    add font

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,5 +1,20 @@
 import { Work_Sans } from "next/font/google";
 
-export const work_sans_bold = Work_Sans({ weight: "700", style: ["normal"] });
-export const work_sans_normal = Work_Sans({ weight: "400", style: ["normal"] });
-export const work_sans_light = Work_Sans({ weight: "300", style: ["normal"] });
+export const work_sans_bold = Work_Sans({
+  weight: "700",
+  style: ["normal"],
+  subsets: ["latin"],
+  preload: false,
+});
+export const work_sans_normal = Work_Sans({
+  weight: "400",
+  style: ["normal"],
+  subsets: ["latin"],
+  preload: false,
+});
+export const work_sans_light = Work_Sans({
+  weight: "300",
+  style: ["normal"],
+  subsets: ["latin"],
+  preload: false,
+});
