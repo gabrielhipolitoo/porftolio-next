cd8490e76fbf87d54a100364a2e62a3924f4487d
gabrielhipolitoo <devgabrielhipolito>
2025-05-03 14:05:52 -0300

    .

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,8 +1,8 @@
 import { Work_Sans } from "next/font/google";
 
-
 export const work_sans = Work_Sans({
-  weight: ["300", "400", "700"], // todos os pesos que você usa
+  weight: ["300", "400", "700"],
   style: ["normal"],
   subsets: ["latin", "latin-ext", "vietnamese"],
+  preload: false,
 });
