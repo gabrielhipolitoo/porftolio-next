a98f6501fe0c998a7b95b3c0f8d442be55100ecb
gabrielhipolitoo <devgabrielhipolito>
2025-05-05 18:06:49 -0300

    .

--- src/components/myName/MyName.tsx
+++ src/components/myName/MyName.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function MyName() {
   return (
     <div className="flex items-center gap-2">
-      <Image className="rounded-lg " alt="icone" src="/assets/images/capa-port.png" width={40} height={40} />
+      <Image className="rounded-lg " alt="icone" src="/assets/images/eu.png" width={40} height={40} />
       <Link href={"/"} className={`text-white font-bold`}>
         GABRIEL HIPÓLITO
       </Link>
