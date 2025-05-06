1f406588742e5474edce9ce832562febba90c6a6
gabrielhipolitoo <devgabrielhipolito>
2025-05-06 17:42:37 -0300

    err build, problem fixed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,14 @@
 import { Composition } from "@/components/section";
+import Link from "next/link";
 import React from "react";
 
 export default function NotFound() {
   return (
     <Composition.section margin="mt-30">
       <Composition.title title="Ops, provavelmente está página ainda esta em construção" />
-      <a href="/"  className=" rounded-3xl p-4 text-2xl text-black bg-amber-50 text-center">
+      <Link href="/"  className=" rounded-3xl p-4 text-2xl text-black bg-amber-50 text-center">
         Voltar para a página inicial
-      </a>
+      </Link>
     </Composition.section>
   );
 }
